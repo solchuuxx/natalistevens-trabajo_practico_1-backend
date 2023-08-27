aa5129a72131b32f233c411b7e1489e316a6e599
solchuuxx <natalistevens1@hotmail.com>
2023-08-27 17:10:41 -0300

    Se hicieron las importaciones, se estableció el archivo .env y se ignoró la carpeta .env y la de node_modules

---
+++ app.js
@@ -0,0 +1,24 @@
+// Importaciones
+const express = require('express');
+const cors = require('cors');
+const helmet = require('helmet');
+const morgan = require('morgan');
+const path = require('path');
+require('dotenv').config();
+
+//Base de datos
+
+const app = express()
+const port = process.env.PORT || 5000
+
+// Middlewares
+app.use(cors());
+app.use(helmet());
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+//Para correr el servidor 
+app.listen(process.env.PORT, ()=>{
+    console.log(`Servidor listo en http://localhost:${process.env.PORT}`)
+})
